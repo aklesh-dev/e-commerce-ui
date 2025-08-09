33a46bfbc825b00a7c83c8dba34624dea5701ec0
aklesh-dev <arzunayavadu998@gmail.com>
2025-08-09 23:31:16 +0545

    Responsive footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div className=''>Footer</div>
-  )
-}
+    <footer className="mt-16 flex flex-col gap-8 items-center md:flex-row md:gap-0 md:items-start md:justify-between bg-gray-800 p-8 rounded-lg">
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href={"/"} className="flex items-center">
+          <Image
+            src={"/logo.png"}
+            alt="Logo"
+            width={36}
+            height={36}
+            className="w-6 h-6 md:w-9 md:h-9"
+          />
+          <span className="hidden md:block font-medium tracking-wider text-white">
+            TRENDIZUMI
+          </span>
+        </Link>
+        <p className="text-gray-400 text-sm">&copy; 2025 TRENDIZUMI</p>
+        <p className="text-gray-400 text-sm">All rights reserved.</p>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>Contact</Link>
+        <Link href={"/"}>Terms of services</Link>
+        <Link href={"/"}>Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Products</p>
+        <Link href={"/"}>All Products</Link>
+        <Link href={"/"}>New Arrivals</Link>
+        <Link href={"/"}>Best Sellers</Link>
+        <Link href={"/"}>Sale</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">About us</p>
+        <Link href={"/"}>About</Link>
+        <Link href={"/"}>Contact</Link>
+        <Link href={"/"}>Blog</Link>
+        <Link href={"/"}>Affiliate Program</Link>
+      </div>
+      
+    </footer>
+  );
+};
 
 export default Footer;
