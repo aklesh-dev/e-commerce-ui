b8820a1f5c5f0b69c5f9d12000ddbe75708c5595
aklesh-dev <arzunayavadu998@gmail.com>
2025-08-17 23:02:47 +0545

    Product Card Design

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,14 +1,64 @@
 "use client";
 
 import { ProductType } from "@/types";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-
-const ProductCard = ({product}:{product: ProductType}) => {
+const ProductCard = ({ product }: { product: ProductType }) => {
   return (
-    <div className='w-full'>
-      product
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      {/* IMAGE */}
+      <Link href={`/products/${product.id}`}>
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={product.images[product.colors[0]]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      </Link>
+      {/* PRODUCT DETAILS */}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium">{product.name}</h1>
+        <p className="text-sm text-gray-500">{product.shortDescription}</p>
+        {/* PRODUCT TYPES */}
+        <div className="flex items-center gap-4 text-xs">
+          {/* SIZES */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Size</span>
+            <select name="size" id="size" className="ring ring-gray-300 rounded-md px-2 py-1">
+              {product.sizes.map((size) => (
+                <option value={size} key={size}>{size.toUpperCase()}</option>
+              ))}
+            </select>
+          </div>
+          {/* COLORS */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Color</span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div className="" key={color}>
+                  <div className="w-[14px] h-[14px] rounded-full" style={{backgroundColor: color}} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* PRICE AND ADD TO CART BUTTON */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium">$ {product.price.toFixed(2)}</p>
+          <button className="ring-1 ring-gray-300 px-2 py-1 shadow-lg rounded-md hover:text-white hover:bg-black transition-colors duration-300 cursor-pointer flex gap-2 items-center">
+            <ShoppingCart size={16}/>
+            Add to Cart
+          </button>
+        </div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductCard;
